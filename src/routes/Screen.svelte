d48a086a2eb687183627f59b0fd42a294419514a
Lautaro <ldelucia@fi.uba.ar>
2023-07-23 17:34:53 -0300

    Toma de Screenshot Simple Terminada

--- src/routes/Screen.svelte
+++ src/routes/Screen.svelte
@@ -2,26 +2,25 @@
 import html2canvas from 'html2canvas';
 import { page } from '$app/stores';
 
-let screenshotUrl = '';
+let screenshotUrl = ''
 
 async function takeScreenshot() {
   const studentEmail = $page.data.session.user?.email;
-  const iframeContent = document.querySelector("iframe").contentDocument.body
-  screenshotUrl = await html2canvas(iframeContent, { useCORS: true }).then(canvas => {
-      return canvas.toDataURL();
-  });
-  const response = await fetch('/api/inTestScreenshot', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 
-      email: studentEmail, // Add the email here
-      imageData: screenshotUrl })
-    });  
-  if (response.ok) {
-    console.log("Data inserted successfully");
-  } else {
-    console.error("Error inserting data:", response.statusText);
-  }
+  html2canvas(document.body, { useCORS: true }).then(async canvas => {
+    screenshotUrl = canvas.toDataURL();    
+    const response = await fetch('/api/inTestScreenshot', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        email: studentEmail,
+        imageData: screenshotUrl })
+    });
+    if (response.ok) {
+      console.log("Data inserted successfully");
+    } else {
+      console.error("Error inserting data:", response.statusText);
+    }
+  });  
 }
 
 </script>
