e25bc897ca4a09a7efacda475ee9e80bafe3c10f
Lautaro <ldelucia@fi.uba.ar>
2023-07-23 19:39:12 -0300

    Se agrega el timing para el screenshot

--- src/routes/Screen.svelte
+++ src/routes/Screen.svelte
@@ -1,30 +1,34 @@
 <script>
-import html2canvas from 'html2canvas';
-import { page } from '$app/stores';
+  import html2canvas from 'html2canvas';
+  import { page } from '$app/stores';
 
-let screenshotUrl = ''
+  let screenshotUrl = '';
 
-async function takeScreenshot() {
-  const studentEmail = $page.data.session.user?.email;
-  html2canvas(document.body, { useCORS: true }).then(async canvas => {
-    screenshotUrl = canvas.toDataURL();    
-    const response = await fetch('/api/inTestScreenshot', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
-        email: studentEmail,
-        imageData: screenshotUrl })
+  async function takeScreenshot() {
+    const studentEmail = $page.data.session.user?.email;
+    html2canvas(document.body, { useCORS: true }).then(async canvas => {
+      screenshotUrl = canvas.toDataURL();
+      const response = await fetch('/api/inTestScreenshot', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: studentEmail, imageData: screenshotUrl })
+      });
+      if (response.ok) {
+        console.log("Data inserted successfully");
+      } else {
+        console.error("Error inserting data:", response.statusText);
+      }
     });
-    if (response.ok) {
-      console.log("Data inserted successfully");
-    } else {
-      console.error("Error inserting data:", response.statusText);
-    }
-  });  
-}
+  }
 
+  // Function to take a screenshot initially and then every minute
+  async function takeScreenshotEveryMinute() {
+    takeScreenshot(); // Take initial screenshot
+    setInterval(takeScreenshot, 60 * 1000); // Take screenshot every minute (60 seconds * 1000 milliseconds)
+  }
+  
+  // Call the function to start taking screenshots every minute when the component is mounted
+  $: takeScreenshotEveryMinute();
 </script>
 
-<!--En un futuro esto hay que hacerlo por tiempo en función de la duración del examen-->
 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdcVUjKVIfcsW1jC2LEKivhJR65bWGRTUkaLo1Nw1N26gWGXQ/viewform?embedded=true" width="100%" height="468" frameborder="0" marginheight="0" marginwidth="0" title="Google Form">Cargando…</iframe>
-<button on:click={takeScreenshot}>Take Screenshot</button>
